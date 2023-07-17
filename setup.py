24517148509873c998e9d7c4c83515fa350c4349
LX-sys <lx984608061@163.com>
2023-07-17 11:56:01 +0800

    PyQtGuiLib-designer 专用组件库

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# @time:2022/12/1018:31
+# @author:LX
+# @file:setup.py
+# @software:PyCharm
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name="PyQtGuiLib-PySide",
+    packages =find_packages(),
+    version="1.0",
+    author="LX",
+    author_email = "lx984608061@163.com",
+    description = "Python version of the qt component library.",
+    long_description=open('README.md', 'r',encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    url = "https://github.com/LX-sys/PyQtGuiLib",
+    requires = ">=3.6",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPL v3",
+        "Operating System :: OS Independent",
+    ]
+)
